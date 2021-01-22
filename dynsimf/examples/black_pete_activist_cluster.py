bbcb42ea55e4646f53f85ef706f634f2daa912d7
Tensaiz <matevy1@gmail.com>
2021-01-22 08:03:06 +0100

    Add more HIOM examples

--- dynsimf/examples/black_pete_activist_cluster.py
+++ dynsimf/examples/black_pete_activist_cluster.py
@@ -12,16 +12,22 @@ from dynsimf.models.components.Memory import MemoryConfigurationType
 
 
 if __name__ == "__main__":
-    n = 400
-    clusters = 10
-    p_within = .2
-    p_between = .001
+    n = 50
+    clusters = 1
+    p_within = 1
+    p_between = 1
     rewiring = .02
 
-    sizes = list(map(int, [n/clusters] * clusters))
-    pm = np.ones((10, 10)) * p_between
+    sizes = list(map(int, [10/clusters] * clusters))
+    pm = np.ones((clusters, clusters)) * p_between
     np.fill_diagonal(pm, p_within)
     g = nx.stochastic_block_model(sizes, pm)
+
+    for i in range(10, 50):
+        g.add_node(i)
+
+    for i in range(10, 50):
+        g.add_edge(i, np.random.randint(0, 50))
 
     cfg = {
         'utility': False,
@@ -33,10 +39,7 @@ if __name__ == "__main__":
     model = Model(g, ModelConfiguration(cfg))
 
     constants = {
-        'link_addition_p': 1,
-        'link_removal_p': 0.00025,
-        'connection_type': 'all', # all / nb_nb
-        'max_links': 15,
+        'max_links': 20,
 
         'N': n,
         'sd_noise_information': .005,
@@ -57,22 +60,23 @@ if __name__ == "__main__":
         np.nan_to_num(x)
         return x
 
-    def information_update(i1,i2,a1,a2,persuasion,r_min,info_update=True):
-        r=r_min+(1-r_min)/(1+np.exp(-1*persuasion*(a1-a2))) # resistance
-        inf=r*i1+(1-r)*i2  # compute weighted man of information
-        if not info_update:
-            inf = i1
-        return inf
-
-    init_I = np.random.normal(.05, 0.1, constants['N'])
+    init_I = np.random.normal(.1, 0, constants['N'])
     init_O = np.random.normal(0, 0.01, constants['N'])
     for _ in range(500):
         init_O = stoch_cusp(constants['N'], init_O, np.zeros(constants['N'])+constants['min_attention'], init_I, constants['s_O'], False)
 
+    # Activism
+    m = np.zeros(constants['N'])
+    m[0:10] = 1
+    information_activists=-1
+    opinion_activists=-1
+
+    init_I[m == 1] = information_activists
+    init_O[m == 1] = opinion_activists
+
     initial_state = {
         'I': init_I,
         'O': init_O,
-        # 'A': 0
     }
 
     def sample_attention_weighted(graph):
@@ -87,13 +91,11 @@ if __name__ == "__main__":
     def get_normalized_links():
         adj = model.get_adjacency()
         link_n = np.sum(adj, axis=1)
-        links_norm = link_n / (15/1.5)
-        # links_norm = links_n / constants['max_links']
+        links_norm = link_n / (constants['max_links']/1.5)
         return links_norm
 
     def update(constants):
         information = model.get_state('I')
-        # attention = model.get_state('A')
         attention = get_normalized_links()
         opinion = model.get_state('O')
 
@@ -102,10 +104,10 @@ if __name__ == "__main__":
         else:
             factor = 1.0/np.sum(attention)
             probs = attention * factor
-            agent = np.random.choice(list(range(constants['N'])), 1, replace=False, p=probs)
+            agent = np.random.choice(list(range(constants['N'])), 1, replace=False, p=probs)[0]
 
         if agent != -1:
-            neighbors = model.get_neighbors(agent[0])
+            neighbors = model.get_neighbors(agent)
             if len(neighbors) > 0:
                 partner = np.random.choice(neighbors)
             else:
@@ -114,49 +116,18 @@ if __name__ == "__main__":
             partner = -1
 
         if partner != -1 and agent != -1:
-            I1 = information[agent]; A1 = attention[agent]; O1 = opinion[agent]
-            I2 = information[partner]; A2 = attention[partner]; O2 = opinion[partner]
+            agent_neighbors = model.get_neighbors(agent).append(partner)
+            agent_neighbors_information = information[agent_neighbors]
+            partner_neighbors = model.get_neighbors(partner).append(agent)
+            partner_neighbors_information = information[partner_neighbors]
 
-            if abs(O1 - O2) < constants['deffuant_c']:
-                information[agent] = information_update(I1,I2,A1,A2,constants['persuasion'],constants['r_min'],True)
-                information[partner] = information_update(I2,I1,A2,A1,constants['persuasion'],constants['r_min'],True)
-
-            # if abs(O1 - O2) < constants['deffuant_c']:
-            #     attention[agent]=attention[agent]+constants['delta_attention']*(2*constants['attention_star']-attention[agent])
-            #     attention[partner]=attention[partner]+constants['delta_attention']*(2*constants['attention_star']-attention[partner])
+            information[agent] = np.average(np.append(agent_neighbors_information, information[agent]))
+            information[partner] = np.average(np.append(partner_neighbors_information, information[partner]))
 
         information = information+np.random.normal(0,constants['sd_noise_information'],constants['N'])
-        # attention = attention-2*constants['delta_attention']*attention/constants['N']   # correction 2 times if interaction
         opinion = stoch_cusp(constants['N'],opinion,attention+constants['min_attention'],information,constants['s_O'], constants['maxwell_convention'])
 
         return {'O': opinion, 'I': information, 'A': attention}
-
-    def activism_states():
-        information = model.get_state('I')
-        opinion = model.get_state('O')
-
-        m = np.zeros(constants['N'])
-        m[np.arange(1, constants['N'], constants['N']/3)[1:].astype(int)-1] = 1
-
-        information_activists=-1
-        opinion_activists=-1
-
-        information[m == 1] = information_activists
-        opinion[m == 1] = opinion_activists
-
-        return {'O': opinion, 'I': information}
-
-    def activism_network():
-        changes = {}
-        activist_nodes = np.arange(1, constants['N'], constants['N']/3)[1:].astype(int)-1
-        adj = model.get_adjacency()
-        for node in activist_nodes:
-            new_links = int(constants['max_links'] - sum(adj[node]))
-            changes[node] = {'add': np.random.choice(np.where(adj[node] == 0)[0], new_links)}
-
-        return {
-            'edge_change': changes
-        }
 
     def network_update(nodes, constants):
         if len(nodes) > 0:
@@ -168,40 +139,31 @@ if __name__ == "__main__":
         update = {}
 
         adj = model.get_adjacency()
-        nb_adj = model.get_neighbors_neighbors_adjacency_matrix()
 
-        # chance for an agent to create a new connection
-        if np.random.random_sample() <= constants['link_addition_p'] and np.sum(adj[node]) < constants['max_links']:
-            if constants['connection_type'] == 'nb_nb':
-                potential_links = np.where(nb_adj[node] == 1)[0] # Neighbors of neighbors
-            elif constants['connection_type'] == 'all':
-                potential_links = np.where(adj[node] == 0)[0]
-
+        # Agent creates a new connection
+        if np.sum(adj[node]) < constants['max_links'] - 1 and np.random.rand() <= 0.5:
+            potential_links = np.where(adj[node] == 0)[0]
+            potential_links = potential_links[np.where(np.sum(adj[potential_links], axis=1) < constants['max_links'])[0]]
             if len(potential_links) > 0:
-                new_link = np.random.choice(potential_links, 1)
+                new_link = np.random.choice(potential_links, 2)
                 update['add'] = new_link
                 changes[node] = update
-            elif len(potential_links) == 0 and constants['connection_type'] == 'nb_nb':
-                # If there are no neighbors of neighbors available add a random new link
-                new_link = np.random.choice(np.where(adj[node] == 1)[0])
-                update['add'] = [new_link]
-                changes[node] = update
 
-        # # remove x percent of links
-        if model.current_iteration <= 7500:
-            removable_links_n = 1
+        # remove x percent of links
+        if model.current_iteration <= 2500 and np.random.rand() <= 0.25:
+            A = 1.5 - get_normalized_links()
+            if max(A) == 0:
+                return []
+            else:
+                factor = 1.0/np.sum(A)
+                probs = (A * factor)
+                node = np.random.choice(np.arange(0, constants['N']), size=1, replace=False, p=probs)[0]
 
-            while removable_links_n > 0:
-                # Pick a random node
-                node = np.random.randint(0, constants['N'])
-                # Pick a link that does exists yet for this agent
+                # Remove a link from an agent
                 neighbors = np.where(adj[node] == 1)[0]
-
                 if len(neighbors) >= 1:
-                    remove_amount = np.random.randint(1, len(neighbors) + 1)
-                    removable_links = np.random.choice(neighbors, remove_amount)
-                    changes[node] = {'remove': removable_links}
-                    removable_links_n -= remove_amount
+                    removable_link = np.random.choice(neighbors, 1)
+                    changes[node] = {'remove': removable_link}
 
         return {
             'edge_change': changes
@@ -221,13 +183,9 @@ if __name__ == "__main__":
     update_network = Update(network_update, update_network_cfg)
     model.add_scheme(Scheme(sample_attention_weighted, {'args': {'graph': model.graph}, 'updates': [update_network]}))
 
-    activism_update = Update(activism_states)
-    activism_network_update = Update(activism_network, UpdateConfiguration({'update_type': UpdateType.NETWORK}))
-    model.add_scheme(Scheme(lambda graph: graph.nodes, {'args': {'graph': model.graph}, 'lower_bound': 300, 'upper_bound': 301, 'updates': [activism_update, activism_network_update]}))
-
     model.set_initial_state(initial_state, {'constants': model.constants})
 
-    output = model.simulate(15000)
+    output = model.simulate(2500)
 
     init_graph = nx.convert_matrix.from_numpy_array(output['adjacency'][0], create_using=nx.DiGraph)
     fixed_positions = nx.drawing.spring_layout(init_graph)
@@ -247,7 +205,7 @@ if __name__ == "__main__":
         'color_scale': 'RdBu',
         'show_plot': False,
         'directed': False,
-        'plot_output': '../animations/HIOM_dynamic.gif',
+        'plot_output': '../animations/Black_pete_cluster_2.gif',
         'plot_title': 'HIERARCHICAL ISING OPINION MODEL',
     }
 
--- dynsimf/examples/black_pete_dynamic_new_I.py
+++ dynsimf/examples/black_pete_dynamic_new_I.py
@@ -33,10 +33,7 @@ if __name__ == "__main__":
     model = Model(g, ModelConfiguration(cfg))
 
     constants = {
-        'link_addition_p': 1,
-        'link_removal_p': 0.00025,
-        'connection_type': 'all', # all / nb_nb
-        'max_links': 15,
+        'max_links': 20,
 
         'N': n,
         'sd_noise_information': .005,
@@ -56,13 +53,6 @@ if __name__ == "__main__":
         x = x-dt*(x**3-b*x-a)+np.random.normal(0,s_O,N)
         np.nan_to_num(x)
         return x
-
-    def information_update(i1,i2,a1,a2,persuasion,r_min,info_update=True):
-        r=r_min+(1-r_min)/(1+np.exp(-1*persuasion*(a1-a2))) # resistance
-        inf=r*i1+(1-r)*i2  # compute weighted man of information
-        if not info_update:
-            inf = i1
-        return inf
 
     init_I = np.random.normal(.05, 0.1, constants['N'])
     init_O = np.random.normal(0, 0.01, constants['N'])
@@ -87,13 +77,11 @@ if __name__ == "__main__":
     def get_normalized_links():
         adj = model.get_adjacency()
         link_n = np.sum(adj, axis=1)
-        links_norm = link_n / (15/1.5)
-        # links_norm = links_n / constants['max_links']
+        links_norm = link_n / (constants['max_links']/1.5)
         return links_norm
 
     def update(constants):
         information = model.get_state('I')
-        # attention = model.get_state('A')
         attention = get_normalized_links()
         opinion = model.get_state('O')
 
@@ -102,10 +90,10 @@ if __name__ == "__main__":
         else:
             factor = 1.0/np.sum(attention)
             probs = attention * factor
-            agent = np.random.choice(list(range(constants['N'])), 1, replace=False, p=probs)
+            agent = np.random.choice(list(range(constants['N'])), 1, replace=False, p=probs)[0]
 
         if agent != -1:
-            neighbors = model.get_neighbors(agent[0])
+            neighbors = model.get_neighbors(agent)
             if len(neighbors) > 0:
                 partner = np.random.choice(neighbors)
             else:
@@ -114,19 +102,15 @@ if __name__ == "__main__":
             partner = -1
 
         if partner != -1 and agent != -1:
-            I1 = information[agent]; A1 = attention[agent]; O1 = opinion[agent]
-            I2 = information[partner]; A2 = attention[partner]; O2 = opinion[partner]
+            agent_neighbors = model.get_neighbors(agent).append(partner)
+            agent_neighbors_information = information[agent_neighbors]
+            partner_neighbors = model.get_neighbors(partner).append(agent)
+            partner_neighbors_information = information[partner_neighbors]
 
-            if abs(O1 - O2) < constants['deffuant_c']:
-                information[agent] = information_update(I1,I2,A1,A2,constants['persuasion'],constants['r_min'],True)
-                information[partner] = information_update(I2,I1,A2,A1,constants['persuasion'],constants['r_min'],True)
-
-            # if abs(O1 - O2) < constants['deffuant_c']:
-            #     attention[agent]=attention[agent]+constants['delta_attention']*(2*constants['attention_star']-attention[agent])
-            #     attention[partner]=attention[partner]+constants['delta_attention']*(2*constants['attention_star']-attention[partner])
+            information[agent] = np.average(np.append(agent_neighbors_information, information[agent]))
+            information[partner] = np.average(np.append(partner_neighbors_information, information[partner]))
 
         information = information+np.random.normal(0,constants['sd_noise_information'],constants['N'])
-        # attention = attention-2*constants['delta_attention']*attention/constants['N']   # correction 2 times if interaction
         opinion = stoch_cusp(constants['N'],opinion,attention+constants['min_attention'],information,constants['s_O'], constants['maxwell_convention'])
 
         return {'O': opinion, 'I': information, 'A': attention}
@@ -151,6 +135,8 @@ if __name__ == "__main__":
         activist_nodes = np.arange(1, constants['N'], constants['N']/3)[1:].astype(int)-1
         adj = model.get_adjacency()
         for node in activist_nodes:
+            if np.sum(adj[node]) == constants['max_links']:
+                continue
             new_links = int(constants['max_links'] - sum(adj[node]))
             changes[node] = {'add': np.random.choice(np.where(adj[node] == 0)[0], new_links)}
 
@@ -168,40 +154,31 @@ if __name__ == "__main__":
         update = {}
 
         adj = model.get_adjacency()
-        nb_adj = model.get_neighbors_neighbors_adjacency_matrix()
 
-        # chance for an agent to create a new connection
-        if np.random.random_sample() <= constants['link_addition_p'] and np.sum(adj[node]) < constants['max_links']:
-            if constants['connection_type'] == 'nb_nb':
-                potential_links = np.where(nb_adj[node] == 1)[0] # Neighbors of neighbors
-            elif constants['connection_type'] == 'all':
-                potential_links = np.where(adj[node] == 0)[0]
-
+        # Agent creates a new connection
+        if np.sum(adj[node]) < constants['max_links'] - 1:
+            potential_links = np.where(adj[node] == 0)[0]
+            potential_links = potential_links[np.where(np.sum(adj[potential_links], axis=1) < constants['max_links'])[0]]
             if len(potential_links) > 0:
-                new_link = np.random.choice(potential_links, 1)
+                new_link = np.random.choice(potential_links, 2)
                 update['add'] = new_link
-                changes[node] = update
-            elif len(potential_links) == 0 and constants['connection_type'] == 'nb_nb':
-                # If there are no neighbors of neighbors available add a random new link
-                new_link = np.random.choice(np.where(adj[node] == 1)[0])
-                update['add'] = [new_link]
                 changes[node] = update
 
         # # remove x percent of links
-        if model.current_iteration <= 7500:
-            removable_links_n = 1
+        if model.current_iteration <= 5000:
+            A = 1.5 - get_normalized_links()
+            if max(A) == 0:
+                return []
+            else:
+                factor = 1.0/np.sum(A)
+                probs = (A * factor)
+                node = np.random.choice(np.arange(0, constants['N']), size=1, replace=False, p=probs)[0]
 
-            while removable_links_n > 0:
-                # Pick a random node
-                node = np.random.randint(0, constants['N'])
-                # Pick a link that does exists yet for this agent
+                # Remove a link from an agent
                 neighbors = np.where(adj[node] == 1)[0]
-
                 if len(neighbors) >= 1:
-                    remove_amount = np.random.randint(1, len(neighbors) + 1)
-                    removable_links = np.random.choice(neighbors, remove_amount)
-                    changes[node] = {'remove': removable_links}
-                    removable_links_n -= remove_amount
+                    removable_link = np.random.choice(neighbors, 1)
+                    changes[node] = {'remove': removable_link}
 
         return {
             'edge_change': changes
@@ -227,7 +204,7 @@ if __name__ == "__main__":
 
     model.set_initial_state(initial_state, {'constants': model.constants})
 
-    output = model.simulate(15000)
+    output = model.simulate(10000)
 
     init_graph = nx.convert_matrix.from_numpy_array(output['adjacency'][0], create_using=nx.DiGraph)
     fixed_positions = nx.drawing.spring_layout(init_graph)
@@ -247,7 +224,7 @@ if __name__ == "__main__":
         'color_scale': 'RdBu',
         'show_plot': False,
         'directed': False,
-        'plot_output': '../animations/HIOM_dynamic.gif',
+        'plot_output': '../animations/HIOM_dynamic_new_I.gif',
         'plot_title': 'HIERARCHICAL ISING OPINION MODEL',
     }
 
